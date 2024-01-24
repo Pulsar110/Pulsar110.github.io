c7fc521bfc211919a464db284650348e845ac915
Pulsar110 <125087940+Pulsar110@users.noreply.github.com>
2024-01-24 18:11:36 -0500

    Create rpg_scripts.js

---
+++ EscapeAI_RPG/rpg_scripts.js
@@ -0,0 +1,19 @@
+function append_user_select_buttons(script_idx) {
+  var btn = $('<button type="button" class="btn btn-secondary"></button>').text('test');
+  $('#user_select').append(btn);
+}
+
+function create_reply_item(response_idx) {
+    var res = document.createElement('p');
+    res.innerHTML = 'test';
+    $('#main_script').append(btn);
+}
+
+
+$(document).ready(function(){
+  append_user_select_buttons(0);
+  create_reply_item(0);
+});
+
+
+
