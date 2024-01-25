8218b2ac1bf909e0d5b0cf76f5b21e4a961d2a0b
Pulsar110 <125087940+Pulsar110@users.noreply.github.com>
2024-01-25 01:16:46 +0000

    fix

--- EscapeAI_RPG/rpg_scripts.js
+++ EscapeAI_RPG/rpg_scripts.js
@@ -8,8 +8,9 @@ function create_reply_item(script_entry) {
 function append_user_select_buttons(json_data, script_idx) {
   $('#user_select').empty();
   $.each(json_data[script_idx]['user'], function(script_subidx, script_text){ 
-    var btn = $('<button type="button" class="btn btn-secondary"></button>').text(script_text);
-    $(btn).on('click', function(){
+    var btn = $('<button type="button" class="btn btn-secondary"></button>');
+    btn.text(script_text);
+    btn.on('click', function(){
       create_reply_item(json_data[script_subidx]);
       append_user_select_buttons(json_data, script_subidx);
     });
@@ -21,7 +22,7 @@ function append_user_select_buttons(json_data, script_idx) {
 $(document).ready(function(){
   $.getJSON('./scripts/0.json', function(json_data) {
     create_reply_item(json_data[0]);
-    append_user_select_buttons(json_data, json_data[0]);
+    append_user_select_buttons(json_data, 0);
   });
 });
 
