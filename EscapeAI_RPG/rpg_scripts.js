88c68f3d4035d11551447b92f206c4c00653f7dd
Pulsar110 <125087940+Pulsar110@users.noreply.github.com>
2024-01-25 03:12:01 +0000

    test

--- EscapeAI_RPG/rpg_scripts.js
+++ EscapeAI_RPG/rpg_scripts.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
   $('body').append('hahahha~');
 
-  $.fn.create_reply_item = function(script_entry) {
-    var res = $('<p></p>').text(script_entry['ai']);
+  $.fn.create_reply_item = function(user_response, context) {
+    var res = $('<p class="user_response"></p>').text(user_response);
     $('#main_script').append(res);
+    $.each(context, function(idx, c){
+      $.each(c, function(res_type, response){
+        var res = $('<p></p>').text(response);
+        res.addClass(res_type);
+        $('#main_script').append(res);
+      });
+    });
   }
 
   $.fn.append_user_select_buttons = function(json_data, script_idx) {
     $('#user_select').empty();
-    $('textarea').html(json_data[script_idx]['user']["1"]);
+    // $('textarea').html(json_data[script_idx]['user']["1"]);
     $.each(json_data[script_idx]['user'], function(script_subidx, script_text){ 
-      var btn = $('<button type="button" class="btn btn-secondary"></button>');
-      btn.html(script_text);
-      btn.on('click', function(){
-        create_reply_item(json_data[script_subidx]);
-        $.fn.append_user_select_buttons(json_data, script_subidx);
+      $.each(script_text, function(short_res, long_res){ 
+        var btn = $('<button type="button" class="btn btn-secondary"></button>');
+        btn.html(short_res);
+        btn.on('click', function(){
+          $('textarea').html(long_res);
+          // create_reply_item(json_data[script_subidx]);
+          // $.fn.append_user_select_buttons(json_data, script_subidx);
+        });
+        $('#user_select').append(btn);
       });
-      $('#user_select').append(btn);
     });
   }
 
